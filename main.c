8db8262be25860d4c6a39e16bc9beb1524a7c862
Massinaam <77203237+Massinaam@users.noreply.github.com>
2021-04-27 21:39:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include "graphe.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int main(void) {
+  clock_t begin = clock();
   // nombre noeuds du graphe (10 pour essayer car petit nb)
+  NOEUD graphe;
   int nbsommets=100;
-  NOEUD graphe = creerGraphe(nbsommets);
-	graphe = implementationGraphe(graphe,nbsommets);
-	// affiche le graphe selon liste adjacence
-	//afficheGraphe(graphe);
-  /*int i=10; 
-  while(i<30)
-  {printf("nombre de voisins de 2%d est %d\n", i, voisin_tiers2(graphe[i]));
-  i++;}*/
- marquage(graphe, nbsommets);
+  int connexe = 1; 
+  while(connexe){
+    graphe = creerGraphe(nbsommets);
+	  graphe = implementationGraphe(graphe,nbsommets);
+	  // affiche le graphe selon liste adjacence
+	  //afficheGraphe(graphe);
+    if(marquage(graphe, nbsommets)){
+      printf("La construction du graphe est un succes, le graphe est connexe\n");
+      connexe = 0;
+    }
+    else{
+      printf("La construction du graphe est un echec, le graphe n'est pas connexe\n");
+      libererGraphe(graphe, nbsommets);
+    }
+  }
+ 
+  graphe=tableRoutage(graphe, nbsommets);
+ 
+  //affiche_noeuds(graphe,nbsommets);
 
- 
-graphe=tableRoutage(graphe, nbsommets);
- 
-//affiche_noeuds(graphe,nbsommets);
- 
-	
-	libererGraphe(graphe, nbsommets);
-	return 0;
+  retrouve_chemin(graphe,nbsommets);
+
+
+  libererGraphe(graphe, nbsommets);
+
+
+
+
+  clock_t end = clock();
+  unsigned long millis = (end -  begin) * 1000/ CLOCKS_PER_SEC;
+  printf("Temps d'execution = %lu ms\n", millis);
+  return 0;
 }
